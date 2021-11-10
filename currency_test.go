7f102766e6b0e243916ae1e15ac051aaebb6c023
Stocco <renato.stoco@gmail.com>
2021-11-10 19:58:57 -0300

    New currencies (#1)
    * Go mod enable (#2)
    
    * find by numeric code
    
    * go fmtted
    
    * Update currency.go
    
    Co-authored-by: Ricardo Medeiros <45370285+r1cm3d@users.noreply.github.com>
    
    * Update currency.go
    
    Co-authored-by: Ricardo Medeiros <45370285+r1cm3d@users.noreply.github.com>
    
    * returning as nil
    
    * go fmteed
    
    * changed map function name
    
    * better map function name
    
    * changing go-mod for compatibility while PR is not merged wyth rhyamond repo
    
    Co-authored-by: Ricardo Medeiros <45370285+r1cm3d@users.noreply.github.com>
    
    * added more currencies and fixed wrong currencies
    
    * testing against iso oficial page
    
    * using pismo package
    
    * go fmt
    
    * removed github flows
    
    Co-authored-by: Ricardo Medeiros <45370285+r1cm3d@users.noreply.github.com>

--- currency_test.go
+++ currency_test.go
@@ -1,6 +1,12 @@
 package money
 
 import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -87,4 +93,147 @@ func TestCurrency_GetNonExistingCurrency(t *testing.T) {
 	if currency != nil {
 		t.Errorf("Unexpected currency returned %+v", currency)
 	}
+}
+
+func TestGetCurrencyByNumericCode(t *testing.T) {
+	type args struct {
+		code string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Currency
+	}{
+		{
+			"happy-currency-find",
+			args{code: "986"},
+			&Currency{Decimal: ",", Thousand: ".", Code: BRL, Fraction: 2, NumericCode: "986", Grapheme: "R$", Template: "$1"},
+		},
+		{
+			"happy-currency-not-found",
+			args{code: "1111"},
+			nil,
+		},
+		{
+			"happy-currency-empty",
+			args{code: ""},
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := CurrencyByNumericCode(tt.args.code)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetCurrencyByNumericCode() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCurrencyCodeByXmlIsoFile(t *testing.T) {
+	xmlIsoFile := getXmlIsoFile()
+	for _, currency := range xmlIsoFile.Table.Items {
+		if currency.CurrencyName != "No universal currency" {
+
+			t.Run(fmt.Sprintf("testing %s", currency.NumericCurrencyCode), func(t *testing.T) {
+				c := CurrencyByNumericCode(currency.NumericCurrencyCode)
+				if !reflect.DeepEqual(c.NumericCode, currency.NumericCurrencyCode) {
+					t.Errorf("GetCurrencyByNumericCode() got = %v, want %v", c.NumericCode, currency.NumericCurrencyCode)
+				}
+			})
+
+			t.Run(fmt.Sprintf("testing %s", currency.CurrencyCode), func(t *testing.T) {
+				c := GetCurrency(currency.CurrencyCode)
+				if !reflect.DeepEqual(c.Code, currency.CurrencyCode) {
+					t.Errorf("GetCurrency() got = %v, want %v", c.Code, currency.CurrencyCode)
+				}
+			})
+
+		}
+	}
+}
+
+const (
+	urlIsoCurrency = "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/amendments/lists/list_one.xml"
+	downloadedFile = "/tmp/downloaded_file.xml"
+	localFile      = "../../../../scripts/currencies/list_one.xml"
+)
+
+type (
+	XmlIsoFile struct {
+		Iso        xml.Name    `xml:"ISO_4217"`
+		Publishing string      `xml:"Pblshd,attr"`
+		Table      IsoCurrency `xml:"CcyTbl"`
+	}
+	IsoCurrency struct {
+		Items []Item `xml:"CcyNtry"`
+	}
+
+	Item struct {
+		Country             string `xml:"CtryNm"`
+		CurrencyName        string `xml:"CcyNm"`
+		CurrencyCode        string `xml:"Ccy"`
+		NumericCurrencyCode string `xml:"CcyNbr"`
+		DecimalUnits        string `xml:"CcyMnrUnts"`
+	}
+)
+
+var xmlIsoFile *XmlIsoFile
+
+func ParseIsoXml() *XmlIsoFile {
+	fileUrl := urlIsoCurrency
+	err := DownloadFile(downloadedFile, fileUrl)
+
+	var xmlFile *os.File
+	if err != nil {
+		fmt.Println("\tUsing local xml file")
+		xmlFile, err = os.Open(localFile)
+	} else {
+		fmt.Println("\tUsing downloads xml file")
+		xmlFile, err = os.Open(downloadedFile)
+	}
+	// if os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	fmt.Println("\tSuccessfully opened list_one.xml")
+	// defer the closing of xmlFile so that we can parse it.
+	defer xmlFile.Close()
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+	// Unmarshal takes a []byte and fills the rss struct with the values found in the xmlFile
+	xml.Unmarshal(byteValue, &xmlIsoFile)
+	fmt.Println("Rss version: " + xmlIsoFile.Publishing)
+
+	return xmlIsoFile
+}
+
+func getXmlIsoFile() *XmlIsoFile {
+	if xmlIsoFile == nil {
+		xmlIsoFile = ParseIsoXml()
+	}
+	return xmlIsoFile
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Rhymond/go-money
+module github.com/pismo/go-money
 
 go 1.13
